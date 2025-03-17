330165ffb22875bcc96ab8c1f37b9cde52a58402
peace <ade12peace@gmail.com>
2025-03-17 13:21:58 +0100

    l

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { productContext } from "../src/contexts/productContext";
+import { productContext } from "../src/contexts/ProductContext";
 
 const Products = () => {
   // const [products, setProducts] = useState([]);
